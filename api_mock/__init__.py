11e13561e88cef5763821bd296dc3f04599dcb31
Quinton Harris <quinton.harris@carbonfive.com>
2021-12-10 19:35:48 -0800

    Remove debug statement

--- api_mock/__init__.py
+++ api_mock/__init__.py
@@ -4,7 +4,6 @@ from .apis import api_blueprint
 from flask_jwt_extended import JWTManager
 
 def create_app() -> Flask:
-    print("creating app")
     app = Flask(__name__)
     app.config["JWT_SECRET_KEY"] = "super secret"
     app.register_blueprint(api_blueprint)
