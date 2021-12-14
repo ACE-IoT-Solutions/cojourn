cfe628179c9eb5a1646a83a2a39d379c8fa8368e
Quinton Harris <quinton.harris@carbonfive.com>
2021-12-13 21:30:06 -0800

    Flesh out device specific tests

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -56,15 +56,25 @@ def test_device_list(authenticated_client):
     device_data = json.loads(response.data)
 
     assert isinstance(device_data, dict)
-    assert device_data['devices'] is not None
-    assert device_data["devices"][0] == thermostat_data
-    assert device_data["devices"][1] == car_charger_data
-    assert device_data["devices"][2] == solar_panels_data
-    assert device_data["devices"][3] == water_heater_data
-    assert device_data["devices"][4] == home_battery_data
+    assert device_data['devices'] == [thermostat_data, car_charger_data, solar_panels_data, water_heater_data, home_battery_data]
 
 def test_device_endpoints(authenticated_client):
-    response = authenticated_client.get("/api/v1/devices/e4d197aa-fa13-4255-b395-63268be12515")
-    device_data = json.loads(response.data)
+    thermostat_response = authenticated_client.get("/api/v1/devices/e4d197aa-fa13-4255-b395-63268be12515")
+    thermostat_response_data = json.loads(thermostat_response.data)
+    assert thermostat_response_data["device"] == thermostat_data
 
-    assert device_data["device"] == thermostat_data
+    car_charger_response = authenticated_client.get("/api/v1/devices/f8204550-32cc-44aa-bf48-a95a90c1504f")
+    car_charger_response_data = json.loads(car_charger_response.data)
+    assert car_charger_response_data["device"] == car_charger_data
+
+    solar_panels_response = authenticated_client.get("/api/v1/devices/8b98e3cf-af15-4ead-8d5e-a2d389723a25")
+    solar_panels_response_data = json.loads(solar_panels_response.data)
+    assert solar_panels_response_data["device"] == solar_panels_data
+    
+    water_heater_response = authenticated_client.get("/api/v1/devices/a3f67bfe-0b41-4054-889e-fa1d6c5d93d1")
+    water_heater_response_data = json.loads(water_heater_response.data)
+    assert water_heater_response_data["device"] == water_heater_data
+
+    home_battery_response = authenticated_client.get("/api/v1/devices/052fe39f-5439-4682-beca-b10f2ea00113")
+    home_battery_response_data = json.loads(home_battery_response.data)
+    assert home_battery_response_data["device"] == home_battery_data
