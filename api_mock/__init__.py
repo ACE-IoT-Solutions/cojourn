b6a228b7d2a3cfa704de485b9866320541f53310
Sevaughn Orr <sevaughnorr@gmail.com>
2022-04-29 21:24:33 +0000

    init config from object
    Signed-off-by: Sevaughn Orr <sevaughnorr@gmail.com>

--- api_mock/__init__.py
+++ api_mock/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask, Blueprint, redirect
 from flask.helpers import url_for
 from api_mock.apis import api_blueprint
-from flask_jwt_extended import JWTManager
+from api_mock.apis.dao.auth_dao import AuthDAO
+from api_mock.apis.protocols import AuthProtocol
+from api_mock.extensions import jwt
+from dotenv import load_dotenv
+from werkzeug.utils import import_string
+from config import Config
 
-def create_app() -> Flask:
+
+def create_app(config: str, auth_dao: AuthProtocol=None) -> Flask:
     app = Flask(__name__)
+    cfg = import_string(f"config.{config}")  
+    app.config.from_object(cfg)
     app.config["JWT_SECRET_KEY"] = "super secret"
     app.register_blueprint(api_blueprint)
     jwt.init_app(app)
--- main.py
+++ main.py
@@ -2,25 +2,60 @@ from optparse import OptionParser
 from api_mock import create_app
 import logging
 
+from http import HTTPStatus
+from api_mock.apis.protocols import AuthProtocol
+
+from api_mock.apis.namespace import auth_ns
+from flask_jwt_extended import create_access_token, get_jwt_identity
+from datetime import timedelta
+
+
+class myDAO(AuthProtocol):
+    def __init__(self):
+        self.tokens = []
+
+    @auth_ns.doc(security=[])
+    def login(self, data):
+        token = create_access_token(data, expires_delta=timedelta(minutes=30))
+        self.tokens.append(token)
+        return {"access_token": token}
+
+    def logout(self):
+        try:
+            self.tokens.remove(get_jwt_identity())
+        except ValueError:
+            auth_ns.abort
+            return HTTPStatus.NOT_FOUND
+
+
 def set_simulator(options, opt_str, value, parser):
-  parser.values.host = "127.0.0.1"
-  parser.values.port = "5000"
+    parser.values.host = "127.0.0.1"
+    parser.values.port = "5000"
+
 
 def set_lan(options, opt_str, value, parser):
-  parser.values.host = "0.0.0.0"
-  parser.values.port = "80"
+    parser.values.host = "0.0.0.0"
+    parser.values.port = "80"
 
-parser = OptionParser()
-parser.add_option("--host", dest="host", default="127.0.0.1", help="Hostname", metavar="HOST")
-parser.add_option("--port", dest="port", default=5000, help="port", metavar="PORT")
-parser.add_option("--simulator", action="callback", callback=set_simulator, nargs=0)
-parser.add_option("--lan", action="callback", callback=set_lan, nargs=0)
 
-(options, args) = parser.parse_args()
+def main():
+    parser = OptionParser()
+    parser.add_option("--host", dest="host", default="127.0.0.1",
+                      help="Hostname", metavar="HOST")
+    parser.add_option("--port", dest="port", default=5000,
+                      help="port", metavar="PORT")
+    parser.add_option("--simulator", action="callback",
+                      callback=set_simulator, nargs=0)
+    parser.add_option("--lan", action="callback", callback=set_lan, nargs=0)
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+    (options, args) = parser.parse_args()
 
-app = create_app()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    app = create_app("ProductionConfig", myDAO())
+    app.run(host=options.host, port=options.port, debug=True)
+
+
 if __name__ == "__main__":
-  app.run(host=options.host, port=options.port, debug=True)
+    main()
