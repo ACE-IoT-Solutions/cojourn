52374d72abb18548cccea6ced0f2878931904956
Andrew <andrew@aceiotsolutions.com>
2022-07-18 02:04:47 -0400

    added changes to work with app engine

--- state/__init__.py
+++ state/__init__.py
@@ -3,12 +3,13 @@ from shutil import copyfile
 import json
 
 def load_state():
-    if not exists('state/state.json'):
-        copyfile('state/example-state.json', 'state/state.json')
+    if not exists('/tmp/state.json'):
+        copyfile('state/example-state.json', '/tmp/state.json')
 
-    with open('state/state.json', 'r') as f:
+    with open('/tmp/state.json', 'r') as f:
         return json.loads(f.read())
 
 def save_state(state):
-    with open('state/state.json', 'w+') as f:
+    with open('/tmp/state.json', 'w+') as f:
         f.write(json.dumps(state, sort_keys=True, indent=2))
+        
