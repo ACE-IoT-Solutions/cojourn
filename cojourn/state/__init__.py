533ee37951ba6c261561b6d3b7b903239d21bed3
Cristian <cmromo.c@gmail.com>
2022-12-20 14:30:29 -0600

    added fix to prevent state from being corrupted

--- cojourn/state/__init__.py
+++ cojourn/state/__init__.py
@@ -3,13 +3,24 @@ from pathlib import Path
 from shutil import copyfile
 import json
 
+def copy_example_state():
+    copyfile(f"{Path(__file__).parent / 'example-state.json'}", 'state.json')
+
 def load_state():
     if not exists('state.json'):
-        copyfile(f"{Path(__file__).parent / 'example-state.json'}", 'state.json')
+        copy_example_state()
 
     with open('state.json', 'r') as f:
-        return json.loads(f.read())
+        data = f.read()
+        return json.loads(data)
 
 def save_state(state):
-    with open('state.json', 'w+') as f:
-        f.write(json.dumps(state, sort_keys=True, indent=2))
+    if not state or len(state) == 0:
+        print("attempting to save empty state. aborting")
+        return
+
+    state_string = json.dumps(state, sort_keys=True, indent=2)
+    if len(state_string) == 0:
+        return
+    with open('state.json', 'w+') as state_file:
+        state_file.write(state_string)
